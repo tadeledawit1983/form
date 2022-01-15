560eecf0632b5d3eaf90b3f711a66987ede17e8d
tadeledawit1983 <tadeledawit1983@gmail.com>
2022-01-15 14:57:07 -0500

    form

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function success(input) {
 }
 
 function validateEmail(email) {
-  const re =
+  let regex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  return regex.test(String(email).toLowerCase());
 }
